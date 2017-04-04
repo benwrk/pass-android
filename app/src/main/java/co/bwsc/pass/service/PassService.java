18144ae7e2f0cc029f4d1a6507b79e02c3240c70
Benjapol Worakan <benwrk@live.com>
2017-04-04 23:03:54 +0700

    Add custom server discovery (experimental)

---
+++ app/src/main/java/co/bwsc/pass/service/PassService.java
@@ -0,0 +1,27 @@
+package co.bwsc.pass.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by Ben on 3/30/2017.
+ */
+
+public class PassService extends Service {
+    public static final String TAG = PassService.class.getSimpleName();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(TAG, "onCreate() called");
+    }
+}
